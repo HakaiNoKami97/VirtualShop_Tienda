50a006246a49f4916e816c06bd7d2355d26c623f
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-20 12:53:17 -0500

    Login de cliente y generar token de autenticacion

--- src/views/LoginApp.vue
+++ src/views/LoginApp.vue
@@ -1,140 +1,165 @@
 <template>
-    <div class="d-flex align-items-center bg-auth border-top border-top-2 border-primary" style="height: 100% !important">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12 col-md-5 col-xl-4 my-5">
-
-                <!-- Heading -->
-                <h1 class="display-4 text-center mb-3">
-                    Iniciar sesión
-                </h1>
-
-                <!-- Subheading -->
-                <p class="text-muted text-center mb-5">
-                   Panel administrador
-                </p>
-
-                <!-- Form -->
-                <form>
-
-                    <div v-if="msm_error" class="alert alert-danger" role="alert">
-                        {{msm_error}}
-                    </div>
-
-
-                    <!-- Email address -->
-                    <div class="form-group">
-
-                    <!-- Label -->
-                    <label class="form-label">
-                        Correo eletrónico
-                    </label>
-
-                    <!-- Input -->
-                    <input type="email" class="form-control" v-model="email" placeholder="name@address.com">
-
-                    </div>
-
-                    <!-- Password -->
-                    <div class="form-group">
-                    <div class="row">
-                        <div class="col">
-
-                        <!-- Label -->
-                        <label class="form-label">
-                            Contraseña
-                        </label>
-
-                        </div>
-                      
-                    </div> <!-- / .row -->
-
-                    <!-- Input group -->
-                    <div class="input-group input-group-merge">
-
-                        <!-- Input -->
-                        <input class="form-control" v-model="password" type="password" placeholder="Enter your password">
-
-                        <!-- Icon -->
-                        <span class="input-group-text">
-                        <i class="fe fe-eye"></i>
-                        </span>
-
-                    </div>
-                    </div>
-
-                    <!-- Submit -->
-                    <button class="btn btn-lg w-100 btn-primary mb-3" type="button" v-on:click="validar()">
-                        Ingresar
-                    </button>
-
-         
-
-                </form>
-
+    <div>
+        <section class="hero" style="margin-top: 11rem;">
+            <div class="container">
+                
+                <div class="hero-content pb-5 text-center">
+                <h1 class="hero-heading mb-0">Customer zone</h1>
                 </div>
-            </div> <!-- / .row -->
-        </div> <!-- / .container -->
+            </div>
+            </section>
+            <!-- customer login-->
+            <section>
+            <div class="container">
+                <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <div class="block">
+                    <div class="block-header">
+                        <h6 class="text-uppercase mb-0">Iniciar sesión</h6>
+                    </div>
+                    <div class="block-body">
+                   
+                        <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                        <hr>
+                        <form action="customer-orders.html" method="get">
+                        <div class="mb-4">
+                            <label class="form-label" for="email1">Correo electrónico</label>
+                            <input class="form-control" id="email1" type="text" placeholder="Correo electrónico" autocomplete="off" v-model="email">
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="password1">Contraseña</label>
+                            <input class="form-control" id="password1" type="password" placeholder="Contraseña" autocomplete="off" v-model="password">
+                        </div>
+
+                        <div class=" mb-4" v-if="msm_error_login">
+                            <small class="text-danger">{{msm_error_login}}</small>
+                        </div>
+
+                        <div class="mb-4 text-center">
+                            <button class="btn btn-outline-dark" type="button" v-on:click="login()"><i class="fa fa-sign-in-alt me-2"></i> Ingresar</button>
+                        </div>
+                        </form>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="block">
+                    <div class="block-header">
+                        <h6 class="text-uppercase mb-0">Nueva cuenta</h6>
+                    </div>
+                    <div class="block-body"> 
+                     
+                        <p class="text-muted">With registration with us new world of fashion, fantastic discounts and much more opens to you! The whole process will not take you more than a minute!</p>
+                    
+                        <form action="customer-orders.html" method="get">
+                        <div class="mb-4">
+                            <label class="form-label" for="name">Nombres completos</label>
+                            <input class="form-control" id="name" type="text" placeholder="Nombres completos" v-model="cliente.nombres">
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="email">Correo electrónico</label>
+                            <input class="form-control" id="email" type="text" placeholder="Correo electrónico" v-model="cliente.email">
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="password">Contraseña</label>
+                            <input class="form-control" id="password" type="password" placeholder="Contraseña" v-model="cliente.password">
+                        </div>
+
+                        <div class=" mb-4" v-if="msm_error">
+                            <small class="text-danger">{{msm_error}}</small>
+                        </div>
+
+                        <div class="mb-4 text-center">
+                            <button class="btn btn-outline-dark" type="button" v-on:click="validar_registro()"><i class="far fa-user me-2"></i>Registrar</button>
+                        </div>
+
+                        
+
+                        </form>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
     </div>
 </template>
 
 <script>
 
 import axios from 'axios';
-import store from '@/store/index';
 
 export default {
-  name: 'LoginApp',
-  data() {
-      return {
-          email: '',
-          password: '',
-          msm_error: ''
-      }
-  },
-  created() {
-      console.log(this.$url);
-  },
-  methods: {
-      validar(){
-        if(!this.email){
-            this.msm_error = 'Ingrese un correo electrónico';
-        }else if(!this.password){
-            this.msm_error = 'Ingrese una contraseña';
-        }else{
-            this.msm_error = '';
-            this.login();
+    name: 'LoginApp',
+    data() {
+        return {
+            cliente: {},
+            msm_error: '',
+
+            email: '',
+            password: '',
+            msm_error_login: ''
         }
-        console.log(this.msm_error);
-      },
+    },
 
-      login(){
-          let data = {
-              email: this.email,
-              password: this.password
-          }
-          axios.post(this.$url+'/login_usuario',data,{
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          }).then((result)=>{
-              console.log(result);
-              if(result.data.data == undefined){
-                  this.msm_error = result.data.message;
-              }
+    methods: {
+        validar_registro(){
+            if(!this.cliente.nombres){
+                this.msm_error = 'Ingrese los nombres por favor.'
+            }else if(!this.cliente.email){
+                this.msm_error = 'Ingrese el correo electrónico por favor.'
+            }else if(!this.cliente.password){
+                this.msm_error = 'Ingrese la contraseña por favor.'
+            }else{
+                //
+                this.msm_error = '';
+                axios.post(this.$url+'/registro_cliente_ecommerce',this.cliente,{
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then((result)=>{
+                    if(result.data.message){
+                        this.msm_error = result.data.message;
+                    }else{
+                        this.msm_error = '';
+                        console.log(result);
+                    }
+                   
+                })
+            }
+            console.log(this.cliente);
+        },
 
-              if(result.data.token){
-                  this.$store.dispatch('saveToken',result.data.token);
+        login(){
+            if(!this.email){
+                this.msm_error_login = 'Ingrese un correo electrónico';
+            }else if(!this.password){
+                this.msm_error_login = 'Ingrese una contraseña';
+            }else{
+                this.msm_error_login = '';
+                let data = {
+                    email: this.email,
+                    password: this.password
+                }
+                axios.post(this.$url+'/login_cliente',data,{
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then((result)=>{
+                    console.log(result);
 
-                  this.$router.push({name: 'dashboard'});
-              }
-          }).catch((error)=>{
-              console.log(error);
-          });
-      }
-  },
-  components: {
+                    if(result.data.message){
+                        this.msm_error_login = result.data.message;
+                    }else{
+                        /* this.$store.dispatch('saveToken',result.data.token);
+                        this.$router.push({name: 'dashboard'}); */
+                    }
 
-  }
+                }).catch((error)=>{
+                    console.log(error);
+                });
+            }
+        }
+    },
 }
 </script>
