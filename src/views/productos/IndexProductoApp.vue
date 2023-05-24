e8b6a8a9f0f775a70d8fc4649ceb12163f81e519
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-23 23:31:41 -0500

    Personalizacion de la tienda y filtros

--- src/views/productos/IndexProductoApp.vue
+++ src/views/productos/IndexProductoApp.vue
@@ -1,22 +1,25 @@
 <template>
-    <div>
-        <section class="hero">
+    <div style="background: #f3f3f3;">
+        <section class="hero" style="margin-top: 8rem !important;">
             <div class="container">
                 <!-- Breadcrumbs -->
                 <ol class="breadcrumb justify-content-center">
-                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                <li class="breadcrumb-item active">Jackets and tops        </li>
+                    <li class="breadcrumb-item">
+                        
+                        <router-link to="/">Inicio</router-link>
+                    </li>
+                    <li class="breadcrumb-item active">Shop        </li>
                 </ol>
                 <!-- Hero Content-->
                 <div class="hero-content pb-5 text-center">
-                <h1 class="hero-heading">Jackets and tops</h1>
+                <h1 class="hero-heading">Tienda</h1>
                 <div class="row">   
                     <div class="col-xl-8 offset-xl-2"><p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p></div>
                 </div>
                 </div>
             </div>
-            </section>
-            <div class="container">
+        </section>
+        <div class="container">
             <div class="row">
                 <!-- Grid -->
                 <div class="products-grid col-xl-9 col-lg-8 order-lg-2">
@@ -247,40 +250,62 @@
                 <!-- / Grid End-->
                 <!-- Sidebar-->
                 <div class="sidebar col-xl-3 col-lg-4 order-lg-1">
-                <div class="sidebar-block px-3 px-lg-0 me-lg-4"><a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#categoriesMenu" aria-expanded="false" aria-controls="categoriesMenu">Product Categories</a>
-                    <div class="expand-lg collapse" id="categoriesMenu">
-                    <div class="nav nav-pills flex-column mt-4 mt-lg-0" role="menu">
-                        <div class="sidebar-menu-item mb-2 active" data-bs-toggle="collapse" data-bs-target="#subcategories_0" aria-expanded="true" aria-controls="subcategories_0" role="menuitem"><a class="nav-link active" href="#!"><span>Jackets</span><span class="sidebar-badge ms-2">123</span></a></div>
-                        <div class="collapse show" id="subcategories_0">
-                        <div class="nav nav-pills flex-column ms-3"><a class="nav-link mb-2" href="#!">Lorem ipsum</a><a class="nav-link mb-2" href="#!">Dolor</a><a class="nav-link mb-2" href="#!">Sit amet</a><a class="nav-link mb-2" href="#!">Donec vitae</a>
+                    <div class="sidebar-block px-3 px-lg-0 me-lg-4"><a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#categoriesMenu" aria-expanded="false" aria-controls="categoriesMenu">Categorias de productos</a>
+                        <div class="expand-lg collapse" id="categoriesMenu">
+                            <div class="nav nav-pills flex-column mt-4 mt-lg-0" role="menu">
+                                <div class="sidebar-menu-item mb-2 active" data-bs-toggle="collapse" data-bs-target="#subcategories_0" aria-expanded="true" aria-controls="subcategories_0" role="menuitem">
+                                    <a class="nav-link active" href="#!">
+                                        <div class="row">
+                                            <div class="col"><span>Jackets</span></div>
+                                            <div class="col" style="text-align: right !important;"><img src="/assets/media/arrow-down.png" style="width:10px" alt=""></div>
+                                        </div>
+                                        
+                                    </a>
+                                </div>
+                                <div class="collapse show" id="subcategories_0">
+                                    <div class="nav nav-pills flex-column ms-3"><a class="nav-link mb-2" href="#!">Lorem ipsum</a><a class="nav-link mb-2" href="#!">Dolor</a><a class="nav-link mb-2" href="#!">Sit amet</a><a class="nav-link mb-2" href="#!">Donec vitae</a>
+                                    </div>
+                                </div>
+                                <div class="sidebar-menu-item mb-2" data-bs-toggle="collapse" data-bs-target="#subcategories_1" aria-expanded="false" aria-controls="subcategories_1" role="menuitem">
+                                    <a class="nav-link " href="#!">
+                                        <div class="row">
+                                            <div class="col"><span>Jackets</span></div>
+                                            <div class="col" style="text-align: right !important;"><img src="/assets/media/arrow-down-dark.png" style="width:10px" alt=""></div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="collapse" id="subcategories_1">
+                                    <div class="nav nav-pills flex-column ms-3"><a class="nav-link mb-2" href="#!">Lorem ipsum</a><a class="nav-link mb-2" href="#!">Dolor</a><a class="nav-link mb-2" href="#!">Sit amet</a><a class="nav-link mb-2" href="#!">Donec vitae</a>
+                                    </div>
+                                </div>
+                                <div class="sidebar-menu-item mb-2" data-bs-toggle="collapse" data-bs-target="#subcategories_2" aria-expanded="false" aria-controls="subcategories_2" role="menuitem">
+                                    <a class="nav-link " href="#!">
+                                        <div class="row">
+                                            <div class="col"><span>Jackets</span></div>
+                                            <div class="col" style="text-align: right !important;"><img src="/assets/media/arrow-down-dark.png" style="width:10px" alt=""></div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="collapse" id="subcategories_2">
+                                    <div class="nav nav-pills flex-column ms-3"><a class="nav-link mb-2" href="#!">Sit amet</a><a class="nav-link mb-2" href="#!">Donec vitae</a><a class="nav-link mb-2" href="#!">Lorem ipsum</a><a class="nav-link mb-2" href="#!">Dolor</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        </div>
-                        <div class="sidebar-menu-item mb-2" data-bs-toggle="collapse" data-bs-target="#subcategories_1" aria-expanded="false" aria-controls="subcategories_1" role="menuitem"><a class="nav-link " href="#!"><span>Jeans &amp; chinos</span><span class="sidebar-badge ms-2">55</span></a></div>
-                        <div class="collapse" id="subcategories_1">
-                        <div class="nav nav-pills flex-column ms-3"><a class="nav-link mb-2" href="#!">Lorem ipsum</a><a class="nav-link mb-2" href="#!">Dolor</a><a class="nav-link mb-2" href="#!">Sit amet</a><a class="nav-link mb-2" href="#!">Donec vitae</a>
-                        </div>
-                        </div>
-                        <div class="sidebar-menu-item mb-2" data-bs-toggle="collapse" data-bs-target="#subcategories_2" aria-expanded="false" aria-controls="subcategories_2" role="menuitem"><a class="nav-link " href="#!"><span>Accessories</span><span class="sidebar-badge ms-2">80</span></a></div>
-                        <div class="collapse" id="subcategories_2">
-                        <div class="nav nav-pills flex-column ms-3"><a class="nav-link mb-2" href="#!">Sit amet</a><a class="nav-link mb-2" href="#!">Donec vitae</a><a class="nav-link mb-2" href="#!">Lorem ipsum</a><a class="nav-link mb-2" href="#!">Dolor</a>
+                    </div>
+                    <div class="sidebar-block px-3 px-lg-0 me-lg-4"><a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#priceFilterMenu" aria-expanded="false" aria-controls="priceFilterMenu">Filter by price</a>
+                        <div class="expand-lg collapse" id="priceFilterMenu">
+                        <h6 class="sidebar-heading d-none d-lg-block">Price  </h6>
+                        <div class="mt-4 mt-lg-0" id="slider-snap"> </div>
+                        <div class="nouislider-values">
+                            <div class="min">From $<span id="slider-snap-value-lower"></span></div>
+                            <div class="max">To $<span id="slider-snap-value-upper"></span></div>
+                            <input class="slider-snap-input" type="hidden" name="pricefrom" id="slider-snap-input-lower" value="40">
+                            <input class="slider-snap-input" type="hidden" name="priceto" id="slider-snap-input-upper" value="110">
                         </div>
                         </div>
                     </div>
-                    </div>
-                </div>
-                <div class="sidebar-block px-3 px-lg-0 me-lg-4"><a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#priceFilterMenu" aria-expanded="false" aria-controls="priceFilterMenu">Filter by price</a>
-                    <div class="expand-lg collapse" id="priceFilterMenu">
-                    <h6 class="sidebar-heading d-none d-lg-block">Price  </h6>
-                    <div class="mt-4 mt-lg-0" id="slider-snap"> </div>
-                    <div class="nouislider-values">
-                        <div class="min">From $<span id="slider-snap-value-lower"></span></div>
-                        <div class="max">To $<span id="slider-snap-value-upper"></span></div>
-                        <input class="slider-snap-input" type="hidden" name="pricefrom" id="slider-snap-input-lower" value="40">
-                        <input class="slider-snap-input" type="hidden" name="priceto" id="slider-snap-input-upper" value="110">
-                    </div>
-                    </div>
-                </div>
-                <div class="sidebar-block px-3 px-lg-0 me-lg-4"><a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#brandFilterMenu" aria-expanded="true" aria-controls="brandFilterMenu">Filter by brand</a>
+                    <div class="sidebar-block px-3 px-lg-0 me-lg-4"><a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#brandFilterMenu" aria-expanded="true" aria-controls="brandFilterMenu">Filter by brand</a>
                     <!-- Brand filter menu - this menu has .show class, so is expanded by default-->
                     <div class="expand-lg collapse show" id="brandFilterMenu">
                     <h6 class="sidebar-heading d-none d-lg-block">Brands </h6>
@@ -317,68 +342,81 @@
                         </div>
                     </form>
                     </div>
-                </div>
-                <div class="sidebar-block px-3 px-lg-0 me-lg-4"> <a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#sizeFilterMenu" aria-expanded="false" aria-controls="sizeFilterMenu">Filter by size</a>
-                    <!-- Size filter menu-->
-                    <div class="expand-lg collapse" id="sizeFilterMenu"> 
-                    <h6 class="sidebar-heading d-none d-lg-block">Size </h6>
-                    <form class="mt-4 mt-lg-0" action="#">  
-                        <div class="mb-1">
-                        <div class="form-check">
-                            <input class="form-check-input" id="size0" type="radio" name="size" checked>
-                            <label class="form-check-label" for="size0">Small</label>
-                        </div>
-                        </div>
-                        <div class="mb-1">
-                        <div class="form-check">
-                            <input class="form-check-input" id="size1" type="radio" name="size">
-                            <label class="form-check-label" for="size1">Medium</label>
-                        </div>
-                        </div>
-                        <div class="mb-1">
-                        <div class="form-check">
-                            <input class="form-check-input" id="size2" type="radio" name="size">
-                            <label class="form-check-label" for="size2">Large</label>
-                        </div>
-                        </div>
-                        <div class="mb-1">
-                        <div class="form-check">
-                            <input class="form-check-input" id="size3" type="radio" name="size">
-                            <label class="form-check-label" for="size3">X-Large</label>
-                        </div>
-                        </div>
-                    </form>
                     </div>
-                </div>
-                <div class="sidebar-block px-3 px-lg-0 me-lg-4"><a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#colourFilterMenu" aria-expanded="false" aria-controls="colourFilterMenu">Filter by colour</a>
-                    <!-- Size filter menu-->
-                    <div class="expand-lg collapse" id="colourFilterMenu">
-                    <h6 class="sidebar-heading d-none d-lg-block">Colour </h6>
-                    <div class="mt-4 mt-lg-0"> 
-                        <ul class="list-inline mb-0 colours-wrapper">
-                        <li class="list-inline-item">
-                            <label class="btn-colour" for="colour_sidebar_Blue" style="background-color: #668cb9" data-allow-multiple> </label>
-                            <input class="input-invisible" type="checkbox" name="colour" value="value_sidebar_Blue" id="colour_sidebar_Blue">
-                        </li>
-                        <li class="list-inline-item">
-                            <label class="btn-colour" for="colour_sidebar_White" style="background-color: #fff" data-allow-multiple> </label>
-                            <input class="input-invisible" type="checkbox" name="colour" value="value_sidebar_White" id="colour_sidebar_White">
-                        </li>
-                        <li class="list-inline-item">
-                            <label class="btn-colour" for="colour_sidebar_Violet" style="background-color: #8b6ea4" data-allow-multiple> </label>
-                            <input class="input-invisible" type="checkbox" name="colour" value="value_sidebar_Violet" id="colour_sidebar_Violet">
-                        </li>
-                        <li class="list-inline-item">
-                            <label class="btn-colour" for="colour_sidebar_Red" style="background-color: #dd6265" data-allow-multiple> </label>
-                            <input class="input-invisible" type="checkbox" name="colour" value="value_sidebar_Red" id="colour_sidebar_Red">
-                        </li>
-                        </ul>
+                    <div class="sidebar-block px-3 px-lg-0 me-lg-4"> <a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#sizeFilterMenu" aria-expanded="false" aria-controls="sizeFilterMenu">Filter by size</a>
+                        <!-- Size filter menu-->
+                        <div class="expand-lg collapse" id="sizeFilterMenu"> 
+                        <h6 class="sidebar-heading d-none d-lg-block">Size </h6>
+                        <form class="mt-4 mt-lg-0" action="#">  
+                            <div class="mb-1">
+                            <div class="form-check">
+                                <input class="form-check-input" id="size0" type="radio" name="size" checked>
+                                <label class="form-check-label" for="size0">Small</label>
+                            </div>
+                            </div>
+                            <div class="mb-1">
+                            <div class="form-check">
+                                <input class="form-check-input" id="size1" type="radio" name="size">
+                                <label class="form-check-label" for="size1">Medium</label>
+                            </div>
+                            </div>
+                            <div class="mb-1">
+                            <div class="form-check">
+                                <input class="form-check-input" id="size2" type="radio" name="size">
+                                <label class="form-check-label" for="size2">Large</label>
+                            </div>
+                            </div>
+                            <div class="mb-1">
+                            <div class="form-check">
+                                <input class="form-check-input" id="size3" type="radio" name="size">
+                                <label class="form-check-label" for="size3">X-Large</label>
+                            </div>
+                            </div>
+                        </form>
+                        </div>
                     </div>
+                    <div class="sidebar-block px-3 px-lg-0 me-lg-4"><a class="d-lg-none block-toggler" data-bs-toggle="collapse" href="#colourFilterMenu" aria-expanded="false" aria-controls="colourFilterMenu">Filter by colour</a>
+                        <!-- Size filter menu-->
+                        <div class="expand-lg collapse" id="colourFilterMenu">
+                        <h6 class="sidebar-heading d-none d-lg-block">Colour </h6>
+                        <div class="mt-4 mt-lg-0"> 
+                            <ul class="list-inline mb-0 colours-wrapper">
+                            <li class="list-inline-item">
+                                <label class="btn-colour" for="colour_sidebar_Blue" style="background-color: #668cb9" data-allow-multiple> </label>
+                                <input class="input-invisible" type="checkbox" name="colour" value="value_sidebar_Blue" id="colour_sidebar_Blue">
+                            </li>
+                            <li class="list-inline-item">
+                                <label class="btn-colour" for="colour_sidebar_White" style="background-color: #fff" data-allow-multiple> </label>
+                                <input class="input-invisible" type="checkbox" name="colour" value="value_sidebar_White" id="colour_sidebar_White">
+                            </li>
+                            <li class="list-inline-item">
+                                <label class="btn-colour" for="colour_sidebar_Violet" style="background-color: #8b6ea4" data-allow-multiple> </label>
+                                <input class="input-invisible" type="checkbox" name="colour" value="value_sidebar_Violet" id="colour_sidebar_Violet">
+                            </li>
+                            <li class="list-inline-item">
+                                <label class="btn-colour" for="colour_sidebar_Red" style="background-color: #dd6265" data-allow-multiple> </label>
+                                <input class="input-invisible" type="checkbox" name="colour" value="value_sidebar_Red" id="colour_sidebar_Red">
+                            </li>
+                            </ul>
+                        </div>
+                        </div>
                     </div>
-                </div>
                 </div>
                 <!-- /Sidebar end-->
             </div>
             </div>
     </div>
 </template>
+
+<style>
+
+.sidebar-menu-item[data-bs-toggle="collapse"]::before{
+    display: none !important;
+}
+
+.nav-pills .nav-link.active, .nav-pills .show>.nav-link {
+    color: #ffffff !important;
+    background-color: #005f96 !important;
+}
+
+</style>
