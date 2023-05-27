1cff76c143d19448699db11b3932859275061339
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-27 16:59:45 -0500

    Personalizar detalle de venta

--- src/views/cuenta/ventas/VentaDetalleApp.vue
+++ src/views/cuenta/ventas/VentaDetalleApp.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-            <section class="hero">
+    <div  style="background: #f3f3f3;" class="pb-5">
+            <section class="hero" style="margin-top: 8rem !important;">
       <div class="container">
         <!-- Breadcrumbs -->
-        <ol class="breadcrumb justify-content-center">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item"><a href="customer-orders.html">Orders</a></li>
-          <li class="breadcrumb-item active">Order #1735        </li>
-        </ol>
+         <ol class="breadcrumb justify-content-center">
+            <li class="breadcrumb-item"><router-link to="/">Home</router-link></li>
+            <li class="breadcrumb-item"><router-link to="/">Mis ventas</router-link></li>
+            <li class="breadcrumb-item active">Orden #123        </li>
+            </ol>
         <!-- Hero Content-->
         <div class="hero-content pb-5 text-center">
           <h1 class="hero-heading">Order #1735</h1>
@@ -23,15 +23,23 @@
           <div class="col-lg-8 col-xl-9">
             <div class="cart">
               <div class="cart-wrapper">
-                <div class="cart-header text-center">
-                  <div class="row">
-                    <div class="col-6">Item</div>
-                    <div class="col-2">Price</div>
-                    <div class="col-2">Quantity</div>
-                    <div class="col-2">Total</div>
-                  </div>
+               <div class="cart-header text-center" style="background: #2983df !important;">
+                    <div class="row">
+                    <div class="col-6">
+                        <span class="text-left text-white">Producto</span>
+                    </div>
+                    <div class="col-2">
+                        <span class="text-white">Precio</span>
+                    </div>
+                    <div class="col-2">
+                        <span class="text-white">Cantidad</span>
+                    </div>
+                    <div class="col-2">
+                        <span class="text-white">Total</span>
+                    </div>
+                    </div>
                 </div>
-                <div class="cart-body">
+                <div class="cart-body" style="background: #fff6e8 !important">
                   <!-- Product-->
                   <div class="cart-item">
                     <div class="row d-flex align-items-center text-center">
@@ -82,11 +90,11 @@
             </div>
             <div class="row my-5">
               <div class="col-md-6">
-                <div class="block mb-5">
-                  <div class="block-header">
+                <div class="block mb-5" >
+                  <div class="block-header" style="background: #fff6e8 !important">
                     <h6 class="text-uppercase mb-0">Order Summary</h6>
                   </div>
-                  <div class="block-body bg-light pt-1">
+                  <div class="block-body bg-light pt-1" style="background: #fff6e8 !important">
                     <p class="text-sm">Shipping and additional costs are calculated based on values you have entered.</p>
                     <ul class="order-summary mb-0 list-unstyled">
                       <li class="order-summary-item"><span>Order Subtotal </span><span>$390.00</span></li>
@@ -98,16 +106,16 @@
                 </div>
               </div>
               <div class="col-md-6">
-                <div class="block-header">
+                <div class="block-header" style="background: #fff6e8 !important">
                   <h6 class="text-uppercase mb-0">Invoice address</h6>
                 </div>
-                <div class="block-body bg-light pt-1">
+                <div class="block-body bg-light pt-1" style="background: #fff6e8 !important">
                   <p>John Brown<br>13/25 New Avenue<br>New Heaven<br>45Y 73J<br>England<br><strong>Great Britain</strong></p>
                 </div>
-                <div class="block-header">
+                <div class="block-header" style="background: #fff6e8 !important">
                   <h6 class="text-uppercase mb-0">Shipping address</h6>
                 </div>
-                <div class="block-body bg-light pt-1">
+                <div class="block-body bg-light pt-1" style="background: #fff6e8 !important">
                   <p>John Brown<br>13/25 New Avenue<br>New Heaven<br>45Y 73J<br>England<br><strong>Great Britain</strong></p>
                 </div>
               </div>
@@ -115,27 +123,7 @@
           </div>
           <!-- Customer Sidebar-->
           <div class="col-xl-3 col-lg-4 mb-5">
-            <div class="customer-sidebar card border-0"> 
-              <div class="customer-profile"><a class="d-inline-block" href="#"><img class="img-fluid rounded-circle customer-image shadow" src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/photo/kyle-loftus-589739-unsplash-avatar.jpg" alt=""></a>
-                <h5>Julie Trnka</h5>
-                <p class="text-muted text-sm mb-0">Ostrava, Czech Republic</p>
-              </div>
-              <nav class="list-group customer-nav"><a class="active list-group-item d-flex justify-content-between align-items-center text-decoration-none" href="customer-orders.html"><span>
-                    <svg class="svg-icon svg-icon-heavy me-2">
-                      <use xlink:href="#paper-bag-1"> </use>
-                    </svg>Orders</span>
-                  <div class="badge rounded-pill bg-light text-dark fw-normal px-3">5</div></a><a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none" href="customer-account.html"><span>
-                    <svg class="svg-icon svg-icon-heavy me-2">
-                      <use xlink:href="#male-user-1"> </use>
-                    </svg>Profile</span></a><a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none" href="customer-addresses.html"><span>
-                    <svg class="svg-icon svg-icon-heavy me-2">
-                      <use xlink:href="#navigation-map-1"> </use>
-                    </svg>Addresses</span></a><a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none" href="customer-login.html"><span>
-                    <svg class="svg-icon svg-icon-heavy me-2">
-                      <use xlink:href="#exit-1"> </use>
-                    </svg>Log out</span></a>
-              </nav>
-            </div>
+              <SidebarCliente/>
           </div>
           <!-- /Customer Sidebar-->
         </div>
@@ -145,7 +133,12 @@
 </template>
 
 <script>
+import SidebarCliente from '@/components/SidebarCliente.vue';
+
 export default {
-    name: 'VentaDetalleApp'
+    name: 'VentaDetalleApp',
+    components: {
+      SidebarCliente
+    }
 }
 </script>
